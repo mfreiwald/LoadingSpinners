abfe56bced6cba1847a67d84875f0d8b67582f8e
Michael Freiwald <michael@freiwald.dev>
2024-03-14 14:52:18 +0100

    Initial Commit

---
+++ LoadingSpinners/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoadingSpinners
+//
+//  Created by Michael on 14.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadingSpinners/LoadingSpinnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingSpinnersApp.swift
+//  LoadingSpinners
+//
+//  Created by Michael on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingSpinnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
